2a657b31b3cbbde0fc7d85b28a977ccc17c8d6ba
Pol Vallverdu <pvallverdualcaide@gmail.com>
2022-07-21 02:00:24 +0200

    Initial Commit

---
+++ src/main/java/engineer/pol/client/PolUtilitiesClient.java
@@ -0,0 +1,13 @@
+package engineer.pol.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PolUtilitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/engineer/pol/polutilities/PolUtilities.java
@@ -0,0 +1,10 @@
+package engineer.pol.polutilities;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PolUtilities implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
