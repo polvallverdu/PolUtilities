bf9a09c723a25328372b5aa8b904c97aa4a87259
Pol Vallverdu <pvallverdualcaide@gmail.com>
2022-07-21 13:52:39 +0200

    Fixed motion command

--- src/main/java/engineer/pol/polutilities/commands/MotionCommand.java
+++ src/main/java/engineer/pol/polutilities/commands/MotionCommand.java
@@ -7,9 +7,12 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.command.argument.Vec3ArgumentType;
+import net.minecraft.entity.Entity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
 import org.apache.logging.log4j.core.tools.picocli.CommandLine;
 
 public class MotionCommand {
@@ -23,11 +26,9 @@ public class MotionCommand {
         LiteralArgumentBuilder<ServerCommandSource> literalBuilder = CommandManager.literal("motion").requires(source -> source.hasPermissionLevel(2));
 
         for (MotionOperation operation : MotionOperation.values()) {
-            literalBuilder.then(CommandManager.literal(operation.name())
+            literalBuilder.then(CommandManager.literal(operation.name().toLowerCase())
                     .then(CommandManager.argument("target", EntityArgumentType.entities())
-                            .then(CommandManager.argument("x", DoubleArgumentType.doubleArg())
-                                .then(CommandManager.argument("y", DoubleArgumentType.doubleArg())
-                                    .then(CommandManager.argument("z", DoubleArgumentType.doubleArg())).executes(context -> motion(context, operation)))))
+                            .then(CommandManager.argument("vel", Vec3ArgumentType.vec3()).executes(context -> motion(context, operation))))
             );
         }
 
@@ -36,20 +37,23 @@ public class MotionCommand {
 
     private static int motion(CommandContext<ServerCommandSource> context, MotionOperation operation) throws CommandSyntaxException {
         var target = EntityArgumentType.getEntities(context, "target");
-        var x = DoubleArgumentType.getDouble(context, "x");
-        var y = DoubleArgumentType.getDouble(context, "y");
-        var z = DoubleArgumentType.getDouble(context, "z");
+        var vel = Vec3ArgumentType.getVec3(context, "vel");
 
         for (var entity : target) {
             switch (operation) {
-                case SET -> entity.setVelocity(x, y, z);
-                case ADD -> entity.setVelocity(entity.getVelocity().add(x, y, z));
+                case SET -> setVelocity(entity, vel);
+                case ADD -> setVelocity(entity, entity.getVelocity().add(vel));
             }
         }
 
         context.getSource().sendFeedback(Text.of("Velocity applied successfully"), false);
 
         return 1;
+    }
+
+    private static void setVelocity(Entity entity, Vec3d vec) {
+        entity.setVelocity(vec);
+        entity.velocityModified = true;
     }
 
 }
